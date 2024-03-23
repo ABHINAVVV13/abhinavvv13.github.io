b8ab373cb208e48b67e3a7c1daca60e22c76e349
abhinavreddy13 <arputta@mun.ca>
2024-03-23 05:56:57 -0230

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { Route,BrowserRouter as Router,Routes} from "react-router-dom"
-import Navbar from "./components/Navbar"
-import { Home,About,Projects,Contact, WalletInfo } from "./pages"
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home, About, Projects, Contact, WalletInfo } from "./pages";
+
 function App() {
   return (
     <main className="bg-slate-300/20">
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/projects' element={<Projects/>}/>
-                <Route path='/contact' element={<Contact/>}/>
-                <Route path='/wallet-info' element={<WalletInfo/>}/>
-
-            </Routes>
-        </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/wallet-info' element={<WalletInfo />} />
+        </Routes>
+      </Router>
     </main>
-  )
+  );
 }
 
-export default App 
+// ..
+export default App;
